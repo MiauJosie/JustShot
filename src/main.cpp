a53fc41aa985500d00a639447364b5df7e6bdcaf
miaujosie <miaujosie@gmail.com>
2026-07-08 17:44:23 -0300

    initial setup

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "raylib.h"
+
+int main()
+{
+    InitWindow(800, 450, "game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, raylib!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
